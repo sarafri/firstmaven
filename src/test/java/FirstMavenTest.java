fd15b6309da42e0a68e36327e280a2d5cb675414
sarafri <sari@friedmango.net>
2025-02-15 21:22:13 +0100

    add a dummy test

---
+++ src/test/java/FirstMavenTest.java
@@ -0,0 +1,10 @@
+public class FirstMavenTest {
+
+    public void testHello(){
+        System.out.println("TEST --- Hello");
+    }
+
+    public void testWorld(){
+        System.out.println("TEST --- World");
+    }
+}
